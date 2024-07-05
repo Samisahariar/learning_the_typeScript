b866d0f6b1542cbd6b4dcf168def0fefc1b43b3c
Samisahariar <samisahariar69@gmail.com>
2024-07-05 08:18:48 -0700

    this is the second commit thig for us

--- build/js/new.js
+++ build/js/new.js
@@ -1,5 +1,23 @@
 "use strict";
-var userName = "sami";
-var meaningofLife;
-var isLoading;
-userName = "jhon";
+//Literal types
+let myName;
+let useName;
+useName = "jhon";
+let multiply = function (c, d) {
+    return c * d;
+};
+const printthis = (a) => {
+    console.log(a);
+};
+//rest prarameters
+const totalnumber = (...nums) => {
+    return nums.reduce((prev, current) => prev + current);
+};
+//never types
+const createError = (errMsg) => {
+    throw new Error(errMsg);
+};
+//number or string
+const numbertocheck = "sami";
+const lengthof = numbertocheck.length;
+//type casting or assertions
--- src/new.ts
+++ src/new.ts
@@ -1,5 +1,45 @@
-let userName : string = "sami";
-let meaningofLife : number;
-let isLoading : boolean;
+type Guitarist = {
+    name: string,
+    age: number,
+    albums: (string | number)[]
+}
 
-userName = "jhon"
+type stringOrNumber = string | number
+
+//Literal types
+let myName: "dave"
+
+let useName: "dave" | "jhon" | "amy"
+
+useName = "jhon"
+
+//fuctions
+
+//type mathfunction = (a : number, b : number) => number
+interface mathfunction {
+    (a: number, b: number): number
+}
+let multiply: mathfunction = function (c, d) {
+    return c * d
+}
+
+const printthis = (a: any) => {
+    console.log(a)
+}
+
+//rest prarameters
+const totalnumber = (...nums: number[]): number => {
+    return nums.reduce((prev, current) => prev + current)
+}
+
+//never types
+const createError = (errMsg: string) => {
+    throw new Error(errMsg)
+}
+
+//number or string
+const numbertocheck: any = "sami"
+const lengthof: number = (numbertocheck as string).length
+
+//type casting or assertions
+ 
